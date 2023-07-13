49cc6b1e9e07809d51e499c893554ce1978f87f7
KaikNunes <kaikleonardo30@gmail.com>
2023-07-13 20:16:10 -0300

    update

--- pagina-inicial-usuario/js/script.js
+++ pagina-inicial-usuario/js/script.js
@@ -9,6 +9,17 @@ function closeModal() {
 }
 
 
+function openModal2() {
+    document.getElementById("modal2").style.display = "block";
+    document.getElementById("overlay").style.display = "block";
+}
+
+function closeModal2() {
+    document.getElementById("modal2").style.display = "none";
+    document.getElementById("overlay").style.display = "none";
+}
+
+
 var projectImage = document.getElementById("project-image");
 var overlay = document.getElementById("overlay2");
 
@@ -19,5 +30,18 @@ projectImage.onclick = function () {
     } else {
         projectImage.classList.add("expanded");
         overlay.style.display = "block";
+    }
+};
+
+var projectImage2 = document.getElementById("project-image2");
+var overlay2 = document.getElementById("overlay3");
+
+projectImage2.onclick = function () {
+    if (projectImage2.classList.contains("expanded")) {
+        projectImage2.classList.remove("expanded");
+        overlay2.style.display = "none";
+    } else {
+        projectImage2.classList.add("expanded");
+        overlay2.style.display = "block";
     }
 };
