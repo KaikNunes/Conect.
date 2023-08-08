5cc264eb2faf0d11f041e9662225be2f02ca60da
KaikNunes <kaikleonardo30@gmail.com>
2023-08-07 21:43:14 -0300

    update

---
+++ firebase-usuario-comum.js
@@ -0,0 +1,19 @@
+function login(event) {
+    event.preventDefault(); // Evita o comportamento padrão de recarregar a página
+
+    var emailInput = document.getElementById('email');
+    var passwordInput = document.getElementById('password');
+
+    if (emailInput && passwordInput) {
+        var email = emailInput.value;
+        var password = passwordInput.value;
+
+        firebase.auth().signInWithEmailAndPassword(email, password)
+        .then(response => {
+            window.location.href = "../../pagina-inicial-usuario-comum/inicial.html";
+        })
+        .catch(error => {
+            alert('Usuário não encontrado');
+        });
+    }
+}
